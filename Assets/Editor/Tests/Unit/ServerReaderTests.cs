bae163f4a3b683371dfb4851fc1733b19eedc41a
Aymeric FAUCHER <afaucher@e3r12p12.42.fr>
2015-12-13 21:47:46 +0100

    More ServerReader Regex

--- Assets/Editor/Tests/Unit/ServerReaderTests.cs
+++ Assets/Editor/Tests/Unit/ServerReaderTests.cs
@@ -84,7 +84,7 @@ public class ServerReaderTests {
 		string test4_string = "seg Thequickbrownfoxjumpsoverthelazydog\nwrtwrtw";
 		string test5_string = "";
 		string test6_string = "string\n";
-		string test7_string = "sog ew\n";
+		string test7_string = "seg \n";
 
 		ServerReader sr = Substitute.For<ServerReader>();
 
@@ -97,6 +97,293 @@ public class ServerReaderTests {
 		bool test6_result = sr.IsGameOverString(test6_string);
 		bool test7_result = sr.IsGameOverString(test7_string);
 
+		// Assert
+		Assert.AreEqual (test1_expected_result, test1_result);
+		Assert.AreEqual (test2_expected_result, test2_result);
+		Assert.AreEqual (test3_expected_result, test3_result);
+		Assert.AreEqual (test4_expected_result, test4_result);
+		Assert.AreEqual (test5_expected_result, test5_result);
+		Assert.AreEqual (test6_expected_result, test6_result);
+		Assert.AreEqual (test7_expected_result, test7_result);
+	}
+
+	[Test]
+	public void IsPlayerDeathString_Regex_Testing()
+	{
+		//Arrange
+		bool test1_expected_result = true;
+		bool test2_expected_result = true;
+		bool test3_expected_result = true;
+		bool test4_expected_result = false;
+		bool test5_expected_result = false;
+		bool test6_expected_result = false;
+		bool test7_expected_result = false;
+		
+		string test1_string = "pdi #0\n";
+		string test2_string = "pdi #99999\n";
+		string test3_string = "pdi #5\n";
+		string test4_string = "pdi #-1\n";
+		string test5_string = "";
+		string test6_string = "string\n";
+		string test7_string = "pdi #\n";
+		
+		ServerReader sr = Substitute.For<ServerReader>();
+		
+		// Act
+		bool test1_result = sr.IsPlayerDeathString(test1_string);
+		bool test2_result = sr.IsPlayerDeathString(test2_string);
+		bool test3_result = sr.IsPlayerDeathString(test3_string);
+		bool test4_result = sr.IsPlayerDeathString(test4_string);
+		bool test5_result = sr.IsPlayerDeathString(test5_string);
+		bool test6_result = sr.IsPlayerDeathString(test6_string);
+		bool test7_result = sr.IsPlayerDeathString(test7_string);
+		
+		// Assert
+		Assert.AreEqual (test1_expected_result, test1_result);
+		Assert.AreEqual (test2_expected_result, test2_result);
+		Assert.AreEqual (test3_expected_result, test3_result);
+		Assert.AreEqual (test4_expected_result, test4_result);
+		Assert.AreEqual (test5_expected_result, test5_result);
+		Assert.AreEqual (test6_expected_result, test6_result);
+		Assert.AreEqual (test7_expected_result, test7_result);
+	}
+
+	[Test]
+	public void IsRottenEggString_Regex_Testing()
+	{
+		//Arrange
+		bool test1_expected_result = true;
+		bool test2_expected_result = true;
+		bool test3_expected_result = true;
+		bool test4_expected_result = false;
+		bool test5_expected_result = false;
+		bool test6_expected_result = false;
+		bool test7_expected_result = false;
+		
+		string test1_string = "edi #0\n";
+		string test2_string = "edi #99999\n";
+		string test3_string = "edi #5\n";
+		string test4_string = "edi #-1\n";
+		string test5_string = "";
+		string test6_string = "string\n";
+		string test7_string = "edi #\n";
+		
+		ServerReader sr = Substitute.For<ServerReader>();
+		
+		// Act
+		bool test1_result = sr.IsRottenEggString(test1_string);
+		bool test2_result = sr.IsRottenEggString(test2_string);
+		bool test3_result = sr.IsRottenEggString(test3_string);
+		bool test4_result = sr.IsRottenEggString(test4_string);
+		bool test5_result = sr.IsRottenEggString(test5_string);
+		bool test6_result = sr.IsRottenEggString(test6_string);
+		bool test7_result = sr.IsRottenEggString(test7_string);
+		
+		// Assert
+		Assert.AreEqual (test1_expected_result, test1_result);
+		Assert.AreEqual (test2_expected_result, test2_result);
+		Assert.AreEqual (test3_expected_result, test3_result);
+		Assert.AreEqual (test4_expected_result, test4_result);
+		Assert.AreEqual (test5_expected_result, test5_result);
+		Assert.AreEqual (test6_expected_result, test6_result);
+		Assert.AreEqual (test7_expected_result, test7_result);
+	}
+
+	[Test]
+	public void IsHatchedEggString_Regex_Testing()
+	{
+		//Arrange
+		bool test1_expected_result = true;
+		bool test2_expected_result = true;
+		bool test3_expected_result = true;
+		bool test4_expected_result = false;
+		bool test5_expected_result = false;
+		bool test6_expected_result = false;
+		bool test7_expected_result = false;
+
+		string test1_string = "eht #0\n";
+		string test2_string = "eht #99999\n";
+		string test3_string = "eht #5\n";
+		string test4_string = "eht #-1\n";
+		string test5_string = "";
+		string test6_string = "string\n";
+		string test7_string = "eht #\n";
+		
+		ServerReader sr = Substitute.For<ServerReader>();
+		
+		// Act
+		bool test1_result = sr.IsHatchedEggString(test1_string);
+		bool test2_result = sr.IsHatchedEggString(test2_string);
+		bool test3_result = sr.IsHatchedEggString(test3_string);
+		bool test4_result = sr.IsHatchedEggString(test4_string);
+		bool test5_result = sr.IsHatchedEggString(test5_string);
+		bool test6_result = sr.IsHatchedEggString(test6_string);
+		bool test7_result = sr.IsHatchedEggString(test7_string);
+		
+		// Assert
+		Assert.AreEqual (test1_expected_result, test1_result);
+		Assert.AreEqual (test2_expected_result, test2_result);
+		Assert.AreEqual (test3_expected_result, test3_result);
+		Assert.AreEqual (test4_expected_result, test4_result);
+		Assert.AreEqual (test5_expected_result, test5_result);
+		Assert.AreEqual (test6_expected_result, test6_result);
+		Assert.AreEqual (test7_expected_result, test7_result);
+	}
+
+	[Test]
+	public void IsEndOfForkString_Regex_Testing()
+	{
+		//Arrange
+		bool test1_expected_result = true;
+		bool test2_expected_result = true;
+		bool test3_expected_result = true;
+		bool test4_expected_result = false;
+		bool test5_expected_result = false;
+		bool test6_expected_result = false;
+		bool test7_expected_result = false;
+		
+		string test1_string = "enw #0 #0 0 0\n";
+		string test2_string = "enw #99999 #99999 99999 99999\n";
+		string test3_string = "enw #1 #1 1 1\n";
+		string test4_string = "enw # #   \n";
+		string test5_string = "";
+		string test6_string = "string\n";
+		string test7_string = "enw #\n";
+		
+		ServerReader sr = Substitute.For<ServerReader>();
+		
+		// Act
+		bool test1_result = sr.IsEndOfForkString(test1_string);
+		bool test2_result = sr.IsEndOfForkString(test2_string);
+		bool test3_result = sr.IsEndOfForkString(test3_string);
+		bool test4_result = sr.IsEndOfForkString(test4_string);
+		bool test5_result = sr.IsEndOfForkString(test5_string);
+		bool test6_result = sr.IsEndOfForkString(test6_string);
+		bool test7_result = sr.IsEndOfForkString(test7_string);
+		
+		// Assert
+		Assert.AreEqual (test1_expected_result, test1_result);
+		Assert.AreEqual (test2_expected_result, test2_result);
+		Assert.AreEqual (test3_expected_result, test3_result);
+		Assert.AreEqual (test4_expected_result, test4_result);
+		Assert.AreEqual (test5_expected_result, test5_result);
+		Assert.AreEqual (test6_expected_result, test6_result);
+		Assert.AreEqual (test7_expected_result, test7_result);
+	}
+
+	[Test]
+	public void IsForkString_Regex_Testing()
+	{
+		//Arrange
+		bool test1_expected_result = true;
+		bool test2_expected_result = true;
+		bool test3_expected_result = true;
+		bool test4_expected_result = false;
+		bool test5_expected_result = false;
+		bool test6_expected_result = false;
+		bool test7_expected_result = false;
+		
+		string test1_string = "pfk #0\n";
+		string test2_string = "pfk #99999\n";
+		string test3_string = "pfk #5\n";
+		string test4_string = "pfk #-1\n";
+		string test5_string = "";
+		string test6_string = "string\n";
+		string test7_string = "pfk #\n";
+		
+		ServerReader sr = Substitute.For<ServerReader>();
+		
+		// Act
+		bool test1_result = sr.IsForkString(test1_string);
+		bool test2_result = sr.IsForkString(test2_string);
+		bool test3_result = sr.IsForkString(test3_string);
+		bool test4_result = sr.IsForkString(test4_string);
+		bool test5_result = sr.IsForkString(test5_string);
+		bool test6_result = sr.IsForkString(test6_string);
+		bool test7_result = sr.IsForkString(test7_string);
+		
+		// Assert
+		Assert.AreEqual (test1_expected_result, test1_result);
+		Assert.AreEqual (test2_expected_result, test2_result);
+		Assert.AreEqual (test3_expected_result, test3_result);
+		Assert.AreEqual (test4_expected_result, test4_result);
+		Assert.AreEqual (test5_expected_result, test5_result);
+		Assert.AreEqual (test6_expected_result, test6_result);
+		Assert.AreEqual (test7_expected_result, test7_result);
+	}
+
+	[Test]
+	public void IsBroadcastMessageString_Regex_Testing()
+	{
+		//Arrange
+		bool test1_expected_result = true;
+		bool test2_expected_result = true;
+		bool test3_expected_result = true;
+		bool test4_expected_result = false;
+		bool test5_expected_result = false;
+		bool test6_expected_result = false;
+		bool test7_expected_result = false;
+		
+		string test1_string = "pbc #0 \"\"\n";
+		string test2_string = "pbc #99999 \"The Quick Brown Fox Jumps Over The Lazy Dog\nAnd that's pretty much all\"\n";
+		string test3_string = "pbc #5 \"Test\"\n";
+		string test4_string = "pbc #-1 \"Test\"\n";
+		string test5_string = "";
+		string test6_string = "string\n";
+		string test7_string = "pbc #1\n";
+		
+		ServerReader sr = Substitute.For<ServerReader>();
+		
+		// Act
+		bool test1_result = sr.IsBroadcastMessageString(test1_string);
+		bool test2_result = sr.IsBroadcastMessageString(test2_string);
+		bool test3_result = sr.IsBroadcastMessageString(test3_string);
+		bool test4_result = sr.IsBroadcastMessageString(test4_string);
+		bool test5_result = sr.IsBroadcastMessageString(test5_string);
+		bool test6_result = sr.IsBroadcastMessageString(test6_string);
+		bool test7_result = sr.IsBroadcastMessageString(test7_string);
+		
+		// Assert
+		Assert.AreEqual (test1_expected_result, test1_result);
+		Assert.AreEqual (test2_expected_result, test2_result);
+		Assert.AreEqual (test3_expected_result, test3_result);
+		Assert.AreEqual (test4_expected_result, test4_result);
+		Assert.AreEqual (test5_expected_result, test5_result);
+		Assert.AreEqual (test6_expected_result, test6_result);
+		Assert.AreEqual (test7_expected_result, test7_result);
+	}
+
+	[Test]
+	public void IsIncantationString_Regex_Testing()
+	{
+		//Arrange
+		bool test1_expected_result = true;
+		bool test2_expected_result = true;
+		bool test3_expected_result = false;
+		bool test4_expected_result = true;
+		bool test5_expected_result = false;
+		bool test6_expected_result = false;
+		bool test7_expected_result = false;
+		
+		string test1_string = "pic 0 0 1 #0\n";
+		string test2_string = "pic 99999 99999 8 #0 #1 #2 #3 #4 #5 #6 #7 #8 #9 #10\n";
+		string test3_string = "pic 99999 99999 8";
+		string test4_string = "pic 5 5 5 #5 #5 #5 #5 #5\n";
+		string test5_string = "";
+		string test6_string = "string\n";
+		string test7_string = "pic #1\n";
+		
+		ServerReader sr = Substitute.For<ServerReader>();
+		
+		// Act
+		bool test1_result = sr.IsIncantationString(test1_string);
+		bool test2_result = sr.IsIncantationString(test2_string);
+		bool test3_result = sr.IsIncantationString(test3_string);
+		bool test4_result = sr.IsIncantationString(test4_string);
+		bool test5_result = sr.IsIncantationString(test5_string);
+		bool test6_result = sr.IsIncantationString(test6_string);
+		bool test7_result = sr.IsIncantationString(test7_string);
+		
 		// Assert
 		Assert.AreEqual (test1_expected_result, test1_result);
 		Assert.AreEqual (test2_expected_result, test2_result);
--- Assets/Scripts/ServerReader.cs
+++ Assets/Scripts/ServerReader.cs
@@ -3,17 +3,17 @@ using System.Collections;
 using System.Text.RegularExpressions;
 
 public class ServerReader : MonoBehaviour {
-	private string X = @"[0-9]*";
-	private string Y = @"[0-9]*";
-	private string q = @"[0-9]*";
-	private string n = @"[0-9]*";
+	private string X = @"[0-9]+";
+	private string Y = @"[0-9]+";
+	private string q = @"[0-9]+";
+	private string n = @"[0-9]+";
 	private string O = @"[1-4]";
 	private string L = @"[1-8]";
-	private string e = @"[0-9]*";
-	private string T = @"[0-9]*";
+	private string e = @"[0-9]+";
+	private string T = @"[0-9]+";
 	private string N = @"\w+";
 	private string R = @"[0-1]";
-	private string M = @"[^\n]*";
+	private string M = "\"[^\"]*\"";
 	private string i = @"[0-6]";
 
 	public bool IsPlayerConnectionString(string serverMessage)
@@ -24,5 +24,40 @@ public class ServerReader : MonoBehaviour {
 	public bool IsGameOverString(string serverMessage)
 	{
 		return Regex.IsMatch (serverMessage, @"seg " + N + "\n$");
+	}
+
+	public bool IsPlayerDeathString(string serverMessage)
+	{
+		return Regex.IsMatch (serverMessage, @"pdi #" + n + "\n$");
+	}
+
+	public bool IsRottenEggString(string serverMessage)
+	{
+		return Regex.IsMatch (serverMessage, @"edi #" + e + "\n$");
+	}
+
+	public bool IsHatchedEggString(string serverMessage)
+	{
+		return Regex.IsMatch (serverMessage, @"eht #" + e + "\n$");
+	}
+
+	public bool IsEndOfForkString(string serverMessage)
+	{
+		return Regex.IsMatch (serverMessage, @"enw #" + e + " #" + n + " " + X + " " + Y + "\n$");
+	}
+
+	public bool IsForkString(string serverMessage)
+	{
+		return Regex.IsMatch (serverMessage, @"pfk #" + n + "\n$");
+	}
+
+	public bool IsBroadcastMessageString(string serverMessage)
+	{
+		return Regex.IsMatch (serverMessage, @"pbc #" + n + " " + M + "\n$");
+	}
+
+	public bool IsIncantationString(string serverMessage)
+	{
+		return Regex.IsMatch (serverMessage, @"pic " + X + " " + Y + " " + L + "( #" + n + ")+\n$");
 	}
 }
