4b93a4d284d3dbc35ad69eb2e3d9e8c7c64a4d2b
Aymeric FAUCHER <afaucher@e3r11p12.42.fr>
2015-12-17 20:22:24 +0100

    debut des tests

---
+++ Assets/Editor/Tests/Unit/PlayerControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using NSubstitute;
+using System.Collections;
+
+[TestFixture]
+public class PlayerControllerTest : MonoBehaviour {
+	[Test]
+	public void Incantate_Test()
+	{
+		// Arrange
+		v
+		// Act
+
+		// Assert
+	}
+
+	public IPlayerMovementController GetPlayerMovementControllerMock()
+	{
+		return Substitute.For<IPlayerMovementController>();
+	}
+
+	public IAnimatorController GetAnimatorControllerMock()
+	{
+	}
+
+	public PlayerController GetPlayerControllerMock(IPlayerMovementController movementController)
+	{
+		var controller = Substitute.For<PlayerController>();
+		controller.SetPlayerMovementController(movementController);
+		return controller;
+	}
+
+	public PlayerController GetPlayerControllerMock(IAnimatorController animatorController)
+	{
+		var controller = Substitute.For<PlayerController>();
+		controller.SetAnimatorController(animatorController);
+		return controller;
+	}
+
+	public PlayerController GetPlayerControllerMock(IAnimatorController animatorController, IPlayerMovementController movementController)
+	{
+		var controller = Substitute.For<PlayerController>();
+		controller.SetAnimatorController(animatorController);
+		controller.SetPlayerMovementController(movementController);
+		return controller;
+	}
+}
