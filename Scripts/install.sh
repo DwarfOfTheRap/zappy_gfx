bd019a750e81a962c48a6d8b764f872fdee84d9b
tdieumeg <tdieumeg@student.42.fr>
2015-12-19 17:05:50 +0100

    new attempt for travis

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,11 +4,11 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
-
-echo 'Installing Unity.pkg'
 if [[ ${TRAVIS_OS_NAME} == osx ]]; then
+    echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
+    curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+
+    echo 'Installing Unity.pkg'
     sudo installer -dumplog -package Unity.pkg -target /
 else
     sudo apt-get update -qq && sudo apt-get install -y unity
