5bf74133d7f717d7f005cfbeb9a4492293126cb8
tdieumeg <tdieumeg@student.42.fr>
2015-12-20 22:05:30 +0100

    Square content UI first try

---
+++ Assets/Scripts/SquareInfoScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SquareInfoScript : MonoBehaviour {
+
+	public ISquare square;
+	public Text linemate;
+	public Text deraumere;
+	public Text sibur;
+	public Text mendiane;
+	public Text phiras;
+	public Text thystame;
+	public Text nourriture;
+	public Text players;
+
+	public void CheckInput()
+	{
+		if (Input.GetMouseButtonUp (0)) {
+			square = OnLeftMouseClick ();
+		}
+		if (Input.GetMouseButtonUp (1)) {
+			GetComponent<CanvasGroup> ().alpha = 0;
+		}
+	}
+
+	public ISquare OnLeftMouseClick()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (hit.collider.gameObject.tag == "Floor1" || hit.collider.gameObject.tag == "Floor2")
+			{
+				GetComponent<CanvasGroup> ().alpha = 1;
+				ISquare square = hit.collider.gameObject.GetComponent<SquareScript>() as ISquare;
+				return square;
+			}
+		}
+		GetComponent<CanvasGroup> ().alpha = 0;
+		return null;
+	}
+
+	void DisplayResources (ISquare square)
+	{
+		linemate.text = square.GetResources ()[SquareContent.LINEMATE].ToString ();
+		deraumere.text = square.GetResources ()[SquareContent.DERAUMERE].ToString ();
+		sibur.text = square.GetResources ()[SquareContent.SIBUR].ToString ();
+		mendiane.text = square.GetResources ()[SquareContent.MENDIANE].ToString ();
+		phiras.text = square.GetResources ()[SquareContent.PHIRAS].ToString ();
+		thystame.text = square.GetResources ()[SquareContent.THYSTAME].ToString ();
+		nourriture.text = square.GetResources ()[SquareContent.NOURRITURE].ToString ();
+		players.text = square.GetResources ()[SquareContent.PLAYERS].ToString ();
+	}
+	
+	void Update () {
+		CheckInput ();
+		if (square != null)
+			DisplayResources(square);
+	}
+}
--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -1,8 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SquareScript : MonoBehaviour, ISquare
 {
+	public Dictionary<SquareContent, int>	resources;
+
+	void Start()
+	{
+		resources = new Dictionary<SquareContent, int> ()
+		{
+			{SquareContent.NOURRITURE, 0},
+			{SquareContent.LINEMATE, 0},
+			{SquareContent.DERAUMERE, 0},
+			{SquareContent.SIBUR, 0},
+			{SquareContent.MENDIANE, 0},
+			{SquareContent.PHIRAS, 0},
+			{SquareContent.THYSTAME, 0},
+			{SquareContent.PLAYERS, 0}
+		};
+	}
+
 	public Vector3 GetPosition ()
 	{
 		return transform.position;
@@ -22,7 +40,7 @@ public class SquareScript : MonoBehaviour, ISquare
 	{
 		return GetComponent<Renderer>().bounds.size.z;
 	}
-	
+
 	public void Destroy ()
 	{
 		Destroy (gameObject);
@@ -32,6 +50,23 @@ public class SquareScript : MonoBehaviour, ISquare
 	{
 		DestroyImmediate (gameObject);
 	}
+
+	public Dictionary<SquareContent, int> GetResources ()
+	{
+		return resources;
+	}
+}
+
+public enum SquareContent
+{
+	NOURRITURE,
+	LINEMATE,
+	DERAUMERE,
+	SIBUR,
+	MENDIANE,
+	PHIRAS,
+	THYSTAME,
+	PLAYERS
 }
 
 public interface ISquare
@@ -42,4 +77,5 @@ public interface ISquare
 	float GetBoundZ();
 	void Destroy();
 	void DestroyImmediate();
+	Dictionary<SquareContent, int> GetResources();
 }
