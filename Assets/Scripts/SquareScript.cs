76a0d7d37a00f6635cc1fed5c7021f6f85120dbe
Aymeric FAUCHER <afaucher@e3r12p11.42.fr>
2015-12-21 18:29:43 +0100

    updating SquareScript for shared use

--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -1,45 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SquareScript : MonoBehaviour, ISquare
 {
-	public Vector3 GetPosition ()
-	{
-		return transform.position;
-	}
-	
-	public float GetBoundX ()
-	{
-		return GetComponent<Renderer>().bounds.size.x;
-	}
-	
-	public float GetBoundY ()
-	{
-		return GetComponent<Renderer>().bounds.size.y;
-	}
-	
-	public float GetBoundZ ()
-	{
-		return GetComponent<Renderer>().bounds.size.z;
-	}
-	
-	public void Destroy ()
-	{
-		Destroy (gameObject);
-	}
+    public SquareContent resources;
 
-	public void DestroyImmediate()
-	{
-		DestroyImmediate (gameObject);
-	}
+    public Vector3 GetPosition ()
+    {
+        return transform.position;
+    }
+    
+    public float GetBoundX ()
+    {
+        return GetComponent<Renderer>().bounds.size.x;
+    }
+    
+    public float GetBoundY ()
+    {
+        return GetComponent<Renderer>().bounds.size.y;
+    }
+    
+    public float GetBoundZ ()
+    {
+        return GetComponent<Renderer>().bounds.size.z;
+    }
+
+    public void Destroy ()
+    {
+        Destroy (gameObject);
+    }
+
+    public void DestroyImmediate()
+    {
+        DestroyImmediate (gameObject);
+    }
+
+    public SquareContent GetResources ()
+    {
+        return resources;
+    }
+}
+
+[System.Serializable]
+public class SquareContent
+{
+    public uint nourriture;
+    public uint linemate;
+    public uint deraumere;
+    public uint sibur;
+    public uint mendiane;
+    public uint phiras;
+    public uint thystame;
+    public List<PlayerController> players;
 }
 
 public interface ISquare
 {
-	Vector3 GetPosition();
-	float GetBoundX();
-	float GetBoundY();
-	float GetBoundZ();
-	void Destroy();
-	void DestroyImmediate();
+    Vector3 GetPosition();
+    float GetBoundX();
+    float GetBoundY();
+    float GetBoundZ();
+    void Destroy();
+    void DestroyImmediate();
+    SquareContent GetResources();
 }
