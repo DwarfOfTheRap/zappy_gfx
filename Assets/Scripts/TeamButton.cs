8f2f888e3525d818230e61a205f5600a3f6ef8cf
Thomas DIEUMEGARD <tdieumeg@e3r11p10.42.fr>
2016-01-10 18:08:48 +0100

    progress

--- Assets/Scripts/TeamButton.cs
+++ Assets/Scripts/TeamButton.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TeamButton : MonoBehaviour {
@@ -7,6 +8,9 @@ public class TeamButton : MonoBehaviour {
 
 	public void ActivateTeamDetails ()
 	{
+		GameObject[]	objs = gameObject.GetComponentsInChildren<Text> ();
+
 		teamDetails.GetComponent<CanvasGroup> ().alpha = 1;
+		teamDetails.GetComponent<TeamDetails> ().team = objs[1].gameObject.GetComponent<TeamCompletion> ().team;
 	}
 }
