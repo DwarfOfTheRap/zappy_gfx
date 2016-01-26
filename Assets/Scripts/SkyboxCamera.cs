78b67102b6c51f7c877e2de8f1e96f645375ece9
Aymeric FAUCHER <afaucher@e3r11p12.42.fr>
2016-01-26 19:00:13 +0100

    new character model

---
+++ Assets/Scripts/SkyboxCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxCamera : MonoBehaviour {
+	public float	speed = 0.0001f;
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localRotation = Quaternion.Euler (this.transform.localRotation.eulerAngles.x, this.transform.localRotation.eulerAngles.y + speed, this.transform.localRotation.eulerAngles.z);
+	}
+}
