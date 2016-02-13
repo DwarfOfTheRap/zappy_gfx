dcd5c74be0f469db1ac6df1d5ca2351ce273c98b
Thomas Dieumegard <thomas.dieumegard@protonmail.com>
2016-02-13 19:36:30 +0100

    after some failing progressing on my tests

--- Assets/Editor/Tests/Unit/NavigationTests.cs
+++ Assets/Editor/Tests/Unit/NavigationTests.cs
@@ -95,6 +95,113 @@ public class NavigationTests {
 		camMov.DidNotReceive().Move (Arg.Is<Vector3>(x => (x.y > initPos.y && x.z < initPos.z)));
 	}
 
+	[Test]
+	public void CameraMoveLeft()
+	{
+		//Arrange
+		ICameraMovement camMov = GetMockCameraMovement ();
+		IInputManager inputM = GetMockInputManager ();
+		CameraController camCon = new CameraController(inputM, camMov, 10, 10);
+		Vector3 initPos;
+		
+		initPos = camCon.position;
+		inputM.MoveLeft().Returns(true);
+		//Act
+		camCon.LateUpdate();
+		//Assert
+		camMov.Received().Move (Arg.Is<Vector3>(x => (x.x < initPos.x)));
+	}
+
+	[Test]
+	public void CameraMoveRight()
+	{
+		//Arrange
+		ICameraMovement camMov = GetMockCameraMovement ();
+		IInputManager inputM = GetMockInputManager ();
+		CameraController camCon = new CameraController(inputM, camMov, 10, 10);
+		Vector3 initPos;
+		
+		initPos = camCon.position;
+		inputM.MoveRight().Returns(true);
+		//Act
+		camCon.LateUpdate();
+		//Assert
+		camMov.Received().Move (Arg.Is<Vector3>(x => (x.x > initPos.x)));
+	}
+
+	[Test]
+	public void CameraMoveForward()
+	{
+		//Arrange
+		ICameraMovement camMov = GetMockCameraMovement ();
+		IInputManager inputM = GetMockInputManager ();
+		CameraController camCon = new CameraController(inputM, camMov, 10, 10);
+		Vector3 initPos;
+		
+		initPos = camCon.position;
+		inputM.MoveForward().Returns(true);
+		inputM.VerticalMovementValue().Returns(1.0f);
+		//Act
+		camCon.LateUpdate();
+		//Assert
+		camMov.Received().Move (Arg.Is<Vector3>(x => (x.z > initPos.z)));
+	}
+
+	[Test]
+	public void CameraMoveBackward()
+	{
+		//Arrange
+		ICameraMovement camMov = GetMockCameraMovement ();
+		IInputManager inputM = GetMockInputManager ();
+		CameraController camCon = new CameraController(inputM, camMov, 10, 10);
+		Vector3 initPos;
+		
+		camCon.position = new Vector3(camCon.position.x, camCon.position.y, 0.0f);
+		initPos = camCon.position;
+		inputM.MoveBackward().Returns(true);
+		inputM.VerticalMovementValue().Returns(-1.0f);
+		//Act
+		camCon.LateUpdate();
+		//Assert
+		camMov.Received().Move (Arg.Is<Vector3>(x => (x.z < initPos.z)));
+	}
+
+	[Test]
+	public void CameraMoveUp()
+	{
+		//Arrange
+		ICameraMovement camMov = GetMockCameraMovement ();
+		IInputManager inputM = GetMockInputManager ();
+		CameraController camCon = new CameraController(inputM, camMov, 10, 10);
+		Vector3 initPos;
+		
+		initPos = camCon.position;
+		inputM.MoveUp().Returns(true);
+		//Act
+		camCon.LateUpdate();
+		//Assert
+		camMov.Received().Move (Arg.Is<Vector3>(x => (x.y > initPos.y)));
+	}
+
+	[Test]
+	public void CameraMoveDown()
+	{
+		//Arrange
+		ICameraMovement camMov = GetMockCameraMovement ();
+		IInputManager inputM = GetMockInputManager ();
+		CameraController camCon = new CameraController(inputM, camMov, 10, 10);
+		Vector3 initPos;
+		
+		initPos = camCon.position;
+		inputM.MoveDown().Returns(true);
+		//Act
+		camCon.LateUpdate();
+		//Assert
+		camMov.Received().Move (Arg.Is<Vector3>(x => (x.y < initPos.y)));
+	}
+
+
+
 	public IInputManager GetMockInputManager()
 	{
 		return Substitute.For<IInputManager> ();
