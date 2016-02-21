69a3e8aa5dcbb0d8ead0904d9cd7275ef4ebe49c
Aymeric FAUCHER <afaucher@e3r11p9.42.fr>
2016-02-22 00:54:02 +0100

    minor quality settings stuff:

--- Assets/ImageEffectManager.cs
+++ Assets/ImageEffectManager.cs
@@ -3,10 +3,18 @@ using System.Collections;
 using UnityStandardAssets.ImageEffects;
 
 public class ImageEffectManager : MonoBehaviour {
+	GameObject directionalLight;
+
+	void Start()
+	{
+		this.directionalLight = GameObject.Find("Directional light");
+	}
 
 	void ManageQualitySettings()
 	{
 		int qualityLevel = QualitySettings.GetQualityLevel();
+		this.directionalLight.SetActive (qualityLevel > 0);
+		GetComponentInChildren<SkyboxCamera>().enabled = qualityLevel > 0;
 		GetComponent<Bloom>().enabled = qualityLevel > 2;
 		GetComponent<VignetteAndChromaticAberration>().enabled = qualityLevel > 3;
 		GetComponent<Antialiasing>().enabled = qualityLevel > 3;
