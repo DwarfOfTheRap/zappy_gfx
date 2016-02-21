41bc8717fc6e168b4fbae280e269ecc5e276d199
Aymeric FAUCHER <afaucher@e3r11p9.42.fr>
2016-02-21 17:47:18 +0100

    Quality Settings + Dynamic Image Effect disabling

---
+++ Assets/ImageEffectManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class ImageEffectManager : MonoBehaviour {
+
+	void ManageQualitySettings()
+	{
+		int qualityLevel = QualitySettings.GetQualityLevel();
+		GetComponent<Bloom>().enabled = qualityLevel > 2;
+		GetComponent<VignetteAndChromaticAberration>().enabled = qualityLevel > 3;
+		GetComponent<Antialiasing>().enabled = qualityLevel > 3;
+		GetComponent<CameraMotionBlur>().enabled = qualityLevel > 4;
+	}
+	void Update()
+	{
+		ManageQualitySettings ();
+	}
+}
