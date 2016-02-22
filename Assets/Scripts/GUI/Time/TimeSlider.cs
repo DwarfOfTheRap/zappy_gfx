43152c5ca17bf658edbf5b0467498cda03c5a557
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-02-22 16:22:43 +0100

    Tme GUI done

---
+++ Assets/Scripts/GUI/Time/TimeSlider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeSlider : MonoBehaviour {
+	
+	private string[]		speeds = {"Stop", "Slow", "Play", "x2", "x4"};
+
+	public void ChangeTextOnEvent(float value)
+	{
+		this.gameObject.GetComponent<Text> ().text = speeds [((int)value)];
+	}
+
+	void Start ()
+	{
+		this.gameObject.GetComponent<Text> ().text = "Play";
+	}
+}
