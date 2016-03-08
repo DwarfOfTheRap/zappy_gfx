28f9dadbf38450fa8881c362b02da94419fa195c
Thomas Dieumegard <Thomas Dieumegard>
2016-03-08 17:32:12 +0100

    Time slider changed

---
+++ Assets/Scripts/GUI/Time/TimeSliderUpdater.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeSliderUpdater : MonoBehaviour {
+
+    public void UpdateSliderOnServerChange(int value)
+    {
+        gameObject.GetComponent<Slider>().value = value;
+        GameManagerScript.instance.timeManager.ChangeTimeSpeed(value);
+    }
+
+    public void UpdateServerOnEvent(float value)
+    {
+        ServerQuery query = new ServerQuery();
+
+        query.SendTimeUnitChangeQuery((int)value);
+    }
+}
