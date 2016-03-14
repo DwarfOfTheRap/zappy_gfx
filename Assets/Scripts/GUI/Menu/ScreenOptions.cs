9778493fddf4d97ce58f7aab52a113af0c288687
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-03-14 23:50:45 +0100

    screen options done

--- Assets/Scripts/GUI/Menu/ScreenOptions.cs
+++ Assets/Scripts/GUI/Menu/ScreenOptions.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 public class ScreenOptions : AWindow {
 
 	private Toggle			_toggle;
-	private Dropdown		_dropdown;
+	private Dropdown		_qualityDropdown;
+	private string[]		_qualities = {"Fastest", "Fast", "Simple", "Good", "Beautiful", "Outrageous"};
+	private Dropdown		_resolutionDropdown;
 	private Resolution[] 	_resolutions = Screen.resolutions;
 
 	protected override void CheckKeyboardShortcut ()
@@ -22,19 +24,37 @@ public class ScreenOptions : AWindow {
 		Screen.SetResolution(_resolutions[value].width, _resolutions[value].height, false);
 	}
 
+	public void OnQualityChangeEvent (int value)
+	{
+		GameManagerScript.instance.qualityManager.ChangeQuality(value);
+	}
+
+	public void OnFullScreenToggle (bool value)
+	{
+		Screen.fullScreen = value;
+		_resolutionDropdown.interactable = !value;
+	}
+
 	protected override void Start ()
 	{
+		int i = 0;
 		_toggle = gameObject.GetComponentInChildren<Toggle>();
-		_dropdown = gameObject.GetComponentInChildren<Dropdown>();
+		_qualityDropdown = GameObject.Find("QualitySettingsDropdown").GetComponent<Dropdown>();
+		_resolutionDropdown = GameObject.Find("ResolutionsDropdown").GetComponent<Dropdown>();
 
 		base.Start();
 		_toggle.isOn = Screen.fullScreen;
-		_dropdown.interactable = !_toggle.isOn;
-		_dropdown.captionText.text = Screen.currentResolution.ToString().Substring(0, 11);
-		for (int i = 0; i < _resolutions.Length; ++i)
+		_qualityDropdown.captionText.text = _qualities[QualityManager.GetQualityLevel()];
+		_qualityDropdown.value = QualityManager.GetQualityLevel();
+		_resolutionDropdown.interactable = !_toggle.isOn;
+		_resolutionDropdown.captionText.text = Screen.width.ToString() + " x " + Screen.height.ToString();
+		while (i < _resolutions.Length)
 		{
-
+			if (_resolutions[i].width == Screen.width)
+				break ;
+			i++;
 		}
+		_resolutionDropdown.value = i;
 	}
 
 }
